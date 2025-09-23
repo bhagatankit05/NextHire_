1874e122600ca0e4ec3ce1c516200b2d8bff8800
bhagatankit05 <bhagatankit82@gmail.com>
2025-09-23 12:51:52 +0530

    feat: update WelcomeContainer to display user name and wrap children in Provider in layout

--- app/(main)/dashboard/_components/WelcomeContainer.jsx
+++ app/(main)/dashboard/_components/WelcomeContainer.jsx
@@ -3,11 +3,11 @@ import { useUser } from '@/app/provider'
 import React from 'react'
 
 const WelcomeContainer = () => {
-    //const {user} = useUser();
+    const {user} = useUser();
   return (
     <div>
      <div>
-       <h2 className='text-lg font-bold'>Welcome Back, {/*user?.name*/}</h2>
+       <h2 className='text-lg font-bold'>Welcome Back, {user?.name}</h2>
        <h2 className='text-secondary'>Seamless Hiring with Intelligent Interview Automation</h2>
 
      </div>
