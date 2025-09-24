3401d8b186709cd8b67dc6f456b645e3db96d016
bhagatankit05 <bhagatankit82@gmail.com>
2025-09-24 13:21:14 +0530

    feat: add InterviewTypes and QUESTIONS_PROMPT for enhanced interview question generation

--- services/Constant.jsx
+++ services/Constant.jsx
@@ -1,4 +1,4 @@
-import { CalendarHeart, LayoutDashboard, List, Settings, WalletCards } from "lucide-react";
+import { BriefcaseBusinessIcon, CalendarHeart, Code2Icon, FileText, GraduationCap, LayoutDashboard, List, Settings, User2Icon, UserCheck, WalletCards } from "lucide-react";
 
 export const SideBarOptions = [
     {
@@ -29,3 +29,56 @@ export const SideBarOptions = [
     },
 
 ]
+
+
+
+export const InterviewTypes = [
+  {
+    title: "Technical Interview",
+    icon: Code2Icon
+  },
+  {
+    title: "Behavioral Interview",
+    icon: User2Icon
+  },
+  {
+    title: "Experienced Hire",
+    icon: BriefcaseBusinessIcon
+  },
+  {
+    title: "HR Interview",
+    icon: UserCheck
+  },
+
+  {
+    title: "Case Study Interview",
+    icon: FileText
+  },
+  {
+    title: "Campus Interview",
+    icon: GraduationCap
+  }
+];
+export const QUESTIONS_PROMPT=`You are an expert technical interviewer.
+Based on the following inputs, generate a well-structured list of high-quality interview questions:
+Job Title: {{jobTitle}}
+Job Description: {{jobDescription}}
+Interview Duration: {{duration}}
+Interview Type: {{type}}
+
+📝 Your task:
+Analyze the job description to identify key responsibilities, required skills, and expected experience.
+Generate a list of interview questions depends on interview duration
+Adjust the number and depth of questions to match the interview duration.
+Ensure the questions match the tone and structure of a real-life {{type}} interview.
+
+🌱 Format your response in JSON format with array list of questions.
+format: interviewQuestions=[
+  {
+    question: "",
+    type: "Technical/Behavioral/Experience/Problem Solving/Leadership"
+  },
+  ...
+]
+
+🎯 The goal is to create a structured, relevant, and time-optimized interview plan for a {{jobTitle}} role.`
